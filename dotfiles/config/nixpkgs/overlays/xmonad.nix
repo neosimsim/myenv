2739e4b845e65189d136c848b1314c98db5afb10
Alexander Ben Nasrallah <me@abn.sh>
2020-11-23 09:20:41 +0100

    move xmonad from tools to myGuiPackages (nix)

--- dotfiles/config/nixpkgs/overlays/xmonad.nix
+++
@@ -1,5 +0,0 @@
-self: super: {
-  myXmonad = self.xmonad-with-packages.override {
-    packages = self: [ self.xmonad-contrib ];
-  };
-}
---
+++ dotfiles/xmonad/shell.nix
@@ -0,0 +1,17 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+let
+  inherit (nixpkgs) pkgs;
+  hsPkgs = if compiler == "default"
+    then pkgs.haskellPackages
+    else pkgs.haskell.packages.${compiler};
+  ghc = hsPkgs.ghcWithPackages (ps: with ps; [
+          xmonad xmonad-contrib
+        ]);
+  hls = hsPkgs.haskell-language-server;
+in
+pkgs.stdenv.mkDerivation {
+  name = "my-haskell-env-0";
+  buildInputs = [ ghc hls ];
+  shellHook = "eval $(egrep ^export ${ghc}/bin/ghc)";
+}
+
