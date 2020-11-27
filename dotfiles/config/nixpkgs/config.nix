915fa35b16674ce7f97a783ebf273bc6c94c0878
Alexander Ben Nasrallah <me@abn.sh>
2020-11-27 09:25:35 +0100

    simple export of PATH and MANPATH
    I don't know why I was convinced that handling PATH in a complex way
    was necessary. I thought this was necessary for nested login shell
    (e.g. when using tmux). But it seems that every distro resets PATH
    in /etc/profile. Maybe I confused the behaviour with MANPATH, which
    is not reset. MANPATH is now reset in $HOME/.profile. Note that MANPATH
    ends with an empty segment, which tells man(1) to also check the system
    default location.

--- dotfiles/config/nixpkgs/config.nix
+++ dotfiles/config/nixpkgs/config.nix
@@ -1,16 +1,8 @@
 {
   packageOverrides = pkgs: with pkgs; rec {
-    myProfile = writeText "my-profile" ''
-      export PATH=$HOME/.nix-profile/bin:$PATH
-      export MANPATH=$HOME/.nix-profile/share/man:$MANPATH
-    '';
     myPackages = pkgs.buildEnv {
       name = "my-packages";
       paths = [
-        (runCommand "profile" {} ''
-          mkdir -p $out/etc/profile.d
-          cp ${myProfile} $out/etc/profile.d/my-profile.sh
-        '')
         jq
         fzf
         plan9port
