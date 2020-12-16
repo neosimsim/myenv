ed0b3efb42e547c7379f4c94990c41175ac7eb67
Alexander Ben Nasrallah <me@abn.sh>
2020-12-17 00:57:34 +0100

    vis: set autoindent

--- dotfiles/config/vis/visrc.lua
+++ dotfiles/config/vis/visrc.lua
@@ -4,6 +4,7 @@ local uni = require('uni')
 vis.events.subscribe(vis.events.INIT, function()
 	-- Your global configuration options
 	vis:command('set theme light-16')
+	vis:command('set autoindent')
         uni:mapKeys()
 end)
 
