cfa218ff9e249c6eaf5aa11417634ac470dae151
Alexander Ben Nasrallah <me@abn.sh>
2020-12-24 00:31:51 +0100

    vis: remove trailing whitespaces on save

--- dotfiles/config/vis/visrc.lua
+++ dotfiles/config/vis/visrc.lua
@@ -8,3 +8,19 @@ end)
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
   vis:command('set autoindent')
 end)
+
+local function rmTrailingBlanks(file, path)
+  local win = vis.win
+  local pos = win.selection.pos
+  local status, out, err = vis:pipe(file, { start = 0, finish = file.size }, 'sed \'s/[[:blank:]]*$//\'')
+  if status ~= 0 or not out then
+   if err then vis:info(err) end
+   return false
+  end
+  file:delete(0, file.size)
+  file:insert(0, out)
+  win.selection.pos = math.min(pos, file.size)
+  return true
+end
+
+vis.events.subscribe(vis.events.FILE_SAVE_PRE, rmTrailingBlanks)
