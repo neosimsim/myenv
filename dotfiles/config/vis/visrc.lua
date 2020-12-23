8dd893c77031292a8a17e9a0de62b24a528587e6
Alexander Ben Nasrallah <me@abn.sh>
2020-12-24 00:31:03 +0100

    vis: set autointend to WIN_OPEN

--- dotfiles/config/vis/visrc.lua
+++ dotfiles/config/vis/visrc.lua
@@ -2,11 +2,9 @@ require('vis')
 local uni = require('uni')
 
 vis.events.subscribe(vis.events.INIT, function()
-  vis:command('set autoindent')
   uni:mapKeys()
 end)
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	-- Your per window configuration options e.g.
-	-- vis:command('set number')
+  vis:command('set autoindent')
 end)
