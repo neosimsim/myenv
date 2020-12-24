7c3c9b262dbc82244a8308678b161758fffa03aa
Alexander Ben Nasrallah <me@abn.sh>
2020-12-25 00:29:44 +0100

    vis: autoformat Haskell with ormolu

--- dotfiles/config/vis/visrc.lua
+++ dotfiles/config/vis/visrc.lua
@@ -11,12 +11,23 @@ end)
 
 local function rmTrailingBlanks(file, path)
   local win = vis.win
-  local pos = win.selection.pos
-  local status, out, err = vis:pipe(file, { start = 0, finish = file.size }, 'sed \'s/[[:blank:]]*$//\'')
-  if status ~= 0 or not out then
-   if err then vis:info(err) end
-   return false
+
+  local fmtCmd = 'sed \'s/[[:blank:]]*$//\''
+  if win.syntax == "haskell" then
+    fmtCmd = "ormolu"
   end
+
+  local pos = win.selection.pos
+  local status, out, err = vis:pipe(file, { start = 0, finish = file.size }, fmtCmd)
+  if status ~= 0 or not out then
+   if err then
+     vis:info(err)
+   else
+     vis:info("error running " .. fmtCmd)
+   end
+   return true
+  end
+
   file:delete(0, file.size)
   file:insert(0, out)
   win.selection.pos = math.min(pos, file.size - 1)
