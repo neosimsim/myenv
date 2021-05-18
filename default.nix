8ebf181b573e49f6148baee88c13e35e1ec0bb87
Alexander Ben Nasrallah <me@abn.sh>
2021-05-18 13:14:43 +0200

    emacs: move config from nix expression to dotfiles

--- default.nix
+++ default.nix
@@ -86,64 +86,6 @@ let
       lsp-mode
       lsp-ui
       lsp-haskell
-
-      (writeTextDir "/share/emacs/site-lisp/default.el" ''
-        (setq create-lockfiles nil)
-        (setq auto-save-default nil)
-        (setq make-backup-files nil)
-        (setq kill-whole-line t)
-        (setq-default show-trailing-whitespace t)
-        (setq-default indicate-empty-lines t)
-        (setq-default indent-tabs-mode nil)
-        (load-theme 'adwaita)
-        (global-font-lock-mode 0)
-        (show-paren-mode)
-
-        (defun pipe-shell-region (cmd start end)
-          "Pipe region to shell command and replace region with the output
-        (combined stdout and stderr). The region is only replaced when the
-        shell command exits 0, otherwise the commands output (combined
-        stdout and stderr) in displayed in a new buffer."
-          (defconst out-buffer (generate-new-buffer "pipe"))
-          (setq exit-status (call-shell-region start end cmd nil out-buffer))
-          (if (equal 0 exit-status)
-              (progn (delete-region start end)
-                     (insert-buffer-substring out-buffer)
-                     (kill-buffer out-buffer))
-            (display-buffer out-buffer)))
-
-        (global-set-key (kbd "C-c C-u") (lambda (&optional start end)
-                                          (interactive "r")
-                                          (pipe-shell-region "uni" start end)))
-
-        (defvar formatter "sed 's/[[:blank:]]*$//'")
-
-        (global-set-key (kbd "C-x M-f") (lambda ()
-                                          (interactive)
-                                          (defconst p (point))
-                                          (pipe-shell-region formatter (point-min) (point-max))
-                                          (goto-char p)))
-
-        (defun haskell-setup ()
-          (setq formatter "ormolu"))
-        (add-hook 'haskell-mode-hook 'haskell-setup)
-
-        (defun cabal-setup ()
-          (setq formatter "cabal-fmt"))
-        (add-hook 'cabal-mode-hook 'cabal-setup)
-
-        (defun elixir-setup ()
-          (setq formatter "mix format"))
-        (add-hook 'elixir-mode-hook 'elixir-setup)
-
-        (defun rust-setup ()
-          (setq formatter "rustfmt"))
-        (add-hook 'rust-mode-hook 'rust-setup)
-
-        (setq lsp-keymap-prefix "C-l")
-        (require 'lsp-mode)
-        (add-hook 'haskell-mode-hook #'lsp)
-      '')
     ]));
 in
 lib.lowPrio (buildEnv {
