e69e5b20027a781355551c7662e25739a0b38cb7
Alexander Ben Nasrallah <me@abn.sh>
2021-05-20 16:03:50 +0200

    scripts: fix shell.nix

--- scripts/shell.nix
+++ scripts/shell.nix
@@ -1,12 +1,12 @@
-{ nixpkgs ? import <nixpkgs> {}, ghc ? "default" }:
+{ pkgs ? import <nixpkgs> {}, ghc ? "default" }:
 let
-  packages = import ./packages.nix { inherit nixpkgs ghc; };
+  packages = import ./packages.nix { inherit pkgs ghc; };
 in
   with packages.haskellPackages;
   shellFor {
     packages = p: [p.scripts];
     withHoogle = true;
-    buildInputs = map nixpkgs.haskell.lib.justStaticExecutables [
+    buildInputs = map pkgs.haskell.lib.justStaticExecutables [
       cabal-install
       steeloverseer
       hlint
