fb03115a9ef05bfded74875d081c62952c546803
Alexander Ben Nasrallah <me@abn.sh>
2021-06-02 16:42:18 +0200

    nix: fix defaultPackage

--- flake.nix
+++ flake.nix
@@ -10,6 +10,6 @@
           my-packages = import self { enableGui = false; pkgs = nixpkgs.legacyPackages.x86_64-linux; };
           my-gui-packages = import self { enableGui = true; pkgs = nixpkgs.legacyPackages.x86_64-linux; };
         };
-      defaultPackage.x86_64-linux = self.packages.legacyPackages.my-packages;
+      defaultPackage.x86_64-linux = self.packages.x86_64-linux.my-packages;
     };
 }
