be4f19a2cfe12508af4fbc11abecc307b29cb191
Alexander Ben Nasrallah <me@abn.sh>
2021-07-12 17:21:22 +0200

    unfix nixpkgs in default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 { enableGui ? false
-, pkgs ? import (builtins.fetchTarball {
-    name = "nixpkgs-unstable";
-    url = "https://github.com/NixOS/nixpkgs/archive/6933d068c5d2fcff398e802f7c4e271bbdab6705.tar.gz";
-    # git ls-remote https://github.com/nixos/nixpkgs nixos-unstable
-    # nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs/archive/6933d068c5d2fcff398e802f7c4e271bbdab6705.tar.gz
-    sha256 = "027axlv5g1l5sxkcnlswhmbc8y0xbgppggg9mhmfb78x7d8anqzq";
-  }) { }
+, pkgs ? import <nixpkgs> {}
 }:
 with pkgs;
 let
