ba47805f64d7cd036c06faaa9d5afa4ae71f82ef
Alexander Ben Nasrallah <me@abn.sh>
2021-11-08 15:04:07 +0100

    nix: update inputs

--- scripts/packages.nix
+++ scripts/packages.nix
@@ -18,15 +18,12 @@ in
   posixScripts = pkgs.stdenv.mkDerivation {
     name = "scripts";
     src = ./.;
-    buildInputs = [ pkgs.plan9port ];
     buildPhase = "true";
     installPhase = ''
-      # ensure PLAN9 is in PATH but at the end so patchShebangs finds
-      # rc(1) but uses GNU sed instead of plan9 sed.
-      . 9; . u
-      patchShebangs Hsfmt
+      sed -i '1c#!${pkgs.plan9port}/plan9/bin/rc' \
+        Hsfmt
+
       make PREFIX=$out install-posix
     '';
   };
 }
-
