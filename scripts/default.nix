f464f6f4f87d70a97f22dd74fdf164a97edf792c
Alexander Ben Nasrallah <me@abn.sh>
2022-02-24 15:27:47 +0100

    scripts add InEmacs

--- scripts/default.nix
+++ scripts/default.nix
@@ -6,11 +6,13 @@ pkgs.stdenv.mkDerivation {
   buildInputs = [ pkgs.makeWrapper ];
 
   installPhase = ''
-    sed -i '1c#!${pkgs.plan9port}/plan9/bin/rc' LN
     make PREFIX=$out install
 
     wrapProgram $out/bin/find-match --set PATH ${with pkgs; lib.makeBinPath [ ripgrep ]}
     wrapProgram $out/bin/find-ex-module --set PATH ${with pkgs; lib.makeBinPath [ ripgrep ]}
     wrapProgram $out/bin/find-ex-function --prefix PATH : ${with pkgs; lib.makeBinPath [ findutils ripgrep ]}
+
+    sed -i '1c#!${pkgs.plan9port}/plan9/bin/rc' $out/bin/InEmacs
+    sed -i '1c#!${pkgs.plan9port}/plan9/bin/rc' $out/bin/LN
   '';
 }
