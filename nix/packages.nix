02ac14a50d041894dc798943d1f969e19688d37c
Alexander Ben Nasrallah <me@abn.sh>
2022-03-03 15:49:16 +0100

    emacs: add smex

--- nix/packages.nix
+++ nix/packages.nix
@@ -92,6 +92,7 @@ let
       (emacsPackagesGen emacs_).withPackages (epkgs: with epkgs; [
         acme-theme
         fzf
+        smex
         highlight-symbol
         buffer-move
         multiple-cursors
