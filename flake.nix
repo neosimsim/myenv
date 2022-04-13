eb6aae4ea3fd6f3323bc737c3f290e225430825f
Alexander Ben Nasrallah <me@abn.sh>
2022-04-13 19:17:48 +0200

    nix: let home-manager follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,7 +3,11 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     hconv = {
       url = "gitlab:neosimsim/hconv";
