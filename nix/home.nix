06f09374b3e8b26a723eb0bb57f0cbff7c0a2a4f
Alexander Ben Nasrallah <me@abn.sh>
2022-04-14 09:09:22 +0200

    nix: disable firefox telemetry

--- nix/home.nix
+++ nix/home.nix
@@ -91,7 +91,34 @@
         # be available in Firefox profiles managed by Home Manager.
         # https://github.com/nix-community/home-manager/blob/e39a9d0103e3b2e42059c986a8c633824b96c193/modules/programs/firefox.nix
         profiles = {
-          default = { };
+          default = {
+            settings = {
+              # disable all sorts of telemetry
+              # https://support.mozilla.org/en-US/questions/1197144
+              "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+              "browser.newtabpage.activity-stream.telemetry" = false;
+              "browser.ping-centre.telemetry" = false;
+              "datareporting.healthreport.service.enabled" = false;
+              "datareporting.healthreport.uploadEnabled" = false;
+              "datareporting.policy.dataSubmissionEnabled" = false;
+              "datareporting.sessions.current.clean" = true;
+              "devtools.onboarding.telemetry.logged" = false;
+              "toolkit.telemetry.archive.enabled" = false;
+              "toolkit.telemetry.bhrPing.enabled" = false;
+              "toolkit.telemetry.enabled" = false;
+              "toolkit.telemetry.firstShutdownPing.enabled" = false;
+              "toolkit.telemetry.hybridContent.enabled" = false;
+              "toolkit.telemetry.newProfilePing.enabled" = false;
+              "toolkit.telemetry.prompted" = 2;
+              "toolkit.telemetry.rejected" = true;
+              "toolkit.telemetry.reportingpolicy.firstRun" = false;
+              "toolkit.telemetry.server" = "";
+              "toolkit.telemetry.shutdownPingSender.enabled" = false;
+              "toolkit.telemetry.unified" = false;
+              "toolkit.telemetry.unifiedIsOptIn" = false;
+              "toolkit.telemetry.updatePing.enabled" = false;
+            };
+          };
         };
 
       };
