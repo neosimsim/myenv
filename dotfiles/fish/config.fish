0a05fa926f1fb9eb2e89e3fcd927c79fd864f9c0
Alexander Ben Nasrallah <me@abn.sh>
2022-04-29 21:16:59 +0200

    fish: copy envs from profile
    fish does not source ~/.profile.

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,4 +1,15 @@
-set fish_greeting
+set -U fish_greeting
+
+set -gx EDITOR emacsclient -tca  ''
+set -gx CDPATH . $HOME $HOME/src
+set -gx GOOS linux
+set -gx GOARCH amd64
+set -gx GOBIN $HOME/bin
+set -gx FZF_DEFAULT_COMMAND fd --type file --follow --hidden --exclude .git
+set -gx FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+
+fish_add_path $HOME/bin
+fish_add_path $HOME/bin/aliases
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
--- nix/home.nix
+++ nix/home.nix
@@ -74,7 +74,6 @@
 
     xdg.configFile = genAttrs
       ([
-        "fish/config.fish"
         "git/attributes"
         "git/config"
         "git/ignore"
@@ -84,120 +83,125 @@
       (name: { source = ../dotfiles + "/${name}"; });
 
     programs =
-      optionalAttrs config.myenv.enableGui
-        {
-          firefox = {
-            enable = true;
+      {
+        fish = {
+          enable = true;
+          shellInit = ''
+            source "${../dotfiles/fish/config.fish}"
+          '';
+        };
+      } // optionalAttrs config.myenv.enableGui {
+        firefox = {
+          enable = true;
 
-            package =
-              let
-                firefoxPkg =
-                  if config.myenv.useSway
-                  then pkgs.firefox-esr-wayland
-                  else pkgs.firefox-esr;
-              in
-              firefoxPkg.override {
-                extraPolicies = {
-                  SearchEngines = {
-                    Default = "DuckDuckGo";
-                    Add = [
-                      {
-                        Name = "Hoogle";
-                        URLTemplate = "https://hoogle.haskell.org/?hoogle={searchTerms}";
-                        Alias = "hoogle";
-                        IconURL = "https://hoogle.haskell.org/favicon.png";
-                      }
-                      {
-                        Name = "Hackage";
-                        URLTemplate = "https://hackage.haskell.org/packages/search?terms={searchTerms}";
-                        Alias = "hackage";
-                        IconURL = "https://hackage.haskell.org/static/favicon.png";
-                      }
-                    ];
-                  };
-                };
-              };
-
-            extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-              ublock-origin
-              vimium
-            ];
-
-            # We need a profile because extensions listed here will only
-            # be available in Firefox profiles managed by Home Manager.
-            # https://github.com/nix-community/home-manager/blob/e39a9d0103e3b2e42059c986a8c633824b96c193/modules/programs/firefox.nix
-            profiles = {
-              default = {
-                settings = {
-                  "signon.rememberSignons" = false;
-
-                  # disable all sorts of telemetry
-                  # https://support.mozilla.org/en-US/questions/1197144
-                  "browser.newtabpage.activity-stream.feeds.telemetry" = false;
-                  "browser.newtabpage.activity-stream.telemetry" = false;
-                  "browser.ping-centre.telemetry" = false;
-                  "datareporting.healthreport.service.enabled" = false;
-                  "datareporting.healthreport.uploadEnabled" = false;
-                  "datareporting.policy.dataSubmissionEnabled" = false;
-                  "datareporting.sessions.current.clean" = true;
-                  "devtools.onboarding.telemetry.logged" = false;
-                  "toolkit.telemetry.archive.enabled" = false;
-                  "toolkit.telemetry.bhrPing.enabled" = false;
-                  "toolkit.telemetry.enabled" = false;
-                  "toolkit.telemetry.firstShutdownPing.enabled" = false;
-                  "toolkit.telemetry.hybridContent.enabled" = false;
-                  "toolkit.telemetry.newProfilePing.enabled" = false;
-                  "toolkit.telemetry.prompted" = 2;
-                  "toolkit.telemetry.rejected" = true;
-                  "toolkit.telemetry.reportingpolicy.firstRun" = false;
-                  "toolkit.telemetry.server" = "";
-                  "toolkit.telemetry.shutdownPingSender.enabled" = false;
-                  "toolkit.telemetry.unified" = false;
-                  "toolkit.telemetry.unifiedIsOptIn" = false;
-                  "toolkit.telemetry.updatePing.enabled" = false;
+          package =
+            let
+              firefoxPkg =
+                if config.myenv.useSway
+                then pkgs.firefox-esr-wayland
+                else pkgs.firefox-esr;
+            in
+            firefoxPkg.override {
+              extraPolicies = {
+                SearchEngines = {
+                  Default = "DuckDuckGo";
+                  Add = [
+                    {
+                      Name = "Hoogle";
+                      URLTemplate = "https://hoogle.haskell.org/?hoogle={searchTerms}";
+                      Alias = "hoogle";
+                      IconURL = "https://hoogle.haskell.org/favicon.png";
+                    }
+                    {
+                      Name = "Hackage";
+                      URLTemplate = "https://hackage.haskell.org/packages/search?terms={searchTerms}";
+                      Alias = "hackage";
+                      IconURL = "https://hackage.haskell.org/static/favicon.png";
+                    }
+                  ];
                 };
               };
             };
-          };
 
-          vscode = {
-            enable = true;
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            ublock-origin
+            vimium
+          ];
 
-            package = pkgs.vscodium;
+          # We need a profile because extensions listed here will only
+          # be available in Firefox profiles managed by Home Manager.
+          # https://github.com/nix-community/home-manager/blob/e39a9d0103e3b2e42059c986a8c633824b96c193/modules/programs/firefox.nix
+          profiles = {
+            default = {
+              settings = {
+                "signon.rememberSignons" = false;
 
-            extensions =
-              let
-                marketplaceExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-                  {
-                    name = "rust";
-                    publisher = "rust-lang";
-                    version = "0.7.8";
-                    sha256 = "sha256-Y33agSNMVmaVCQdYd5mzwjiK5JTZTtzTkmSGTQrSNg0=";
-                  }
-                ];
-              in
-              with pkgs.vscode-extensions; [
-                haskell.haskell
-                justusadam.language-haskell
-                jnoortheen.nix-ide
-                elixir-lsp.vscode-elixir-ls
-                dbaeumer.vscode-eslint
-              ] ++ marketplaceExtensions;
-
-            userSettings = {
-              "workbench.colorTheme" = "Default Light+";
-              "nix.enableLanguageServer" = true;
-              "terminal.integrated.env.linux" = {
-                "EDITOR" = "codium -w";
+                # disable all sorts of telemetry
+                # https://support.mozilla.org/en-US/questions/1197144
+                "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+                "browser.newtabpage.activity-stream.telemetry" = false;
+                "browser.ping-centre.telemetry" = false;
+                "datareporting.healthreport.service.enabled" = false;
+                "datareporting.healthreport.uploadEnabled" = false;
+                "datareporting.policy.dataSubmissionEnabled" = false;
+                "datareporting.sessions.current.clean" = true;
+                "devtools.onboarding.telemetry.logged" = false;
+                "toolkit.telemetry.archive.enabled" = false;
+                "toolkit.telemetry.bhrPing.enabled" = false;
+                "toolkit.telemetry.enabled" = false;
+                "toolkit.telemetry.firstShutdownPing.enabled" = false;
+                "toolkit.telemetry.hybridContent.enabled" = false;
+                "toolkit.telemetry.newProfilePing.enabled" = false;
+                "toolkit.telemetry.prompted" = 2;
+                "toolkit.telemetry.rejected" = true;
+                "toolkit.telemetry.reportingpolicy.firstRun" = false;
+                "toolkit.telemetry.server" = "";
+                "toolkit.telemetry.shutdownPingSender.enabled" = false;
+                "toolkit.telemetry.unified" = false;
+                "toolkit.telemetry.unifiedIsOptIn" = false;
+                "toolkit.telemetry.updatePing.enabled" = false;
               };
             };
-          };
-        } // optionalAttrs config.myenv.useSway
-        {
-          waybar = {
-            enable = true;
           };
         };
+
+        vscode = {
+          enable = true;
+
+          package = pkgs.vscodium;
+
+          extensions =
+            let
+              marketplaceExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+                {
+                  name = "rust";
+                  publisher = "rust-lang";
+                  version = "0.7.8";
+                  sha256 = "sha256-Y33agSNMVmaVCQdYd5mzwjiK5JTZTtzTkmSGTQrSNg0=";
+                }
+              ];
+            in
+            with pkgs.vscode-extensions; [
+              haskell.haskell
+              justusadam.language-haskell
+              jnoortheen.nix-ide
+              elixir-lsp.vscode-elixir-ls
+              dbaeumer.vscode-eslint
+            ] ++ marketplaceExtensions;
+
+          userSettings = {
+            "workbench.colorTheme" = "Default Light+";
+            "nix.enableLanguageServer" = true;
+            "terminal.integrated.env.linux" = {
+              "EDITOR" = "codium -w";
+            };
+          };
+        };
+      } // optionalAttrs config.myenv.useSway {
+        waybar = {
+          enable = true;
+        };
+      };
 
     wayland.windowManager.sway = optionalAttrs config.myenv.useSway {
       enable = true;
