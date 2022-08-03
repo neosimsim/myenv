77395d77090141e5302cf182bed058abd609f590
Alexander Ben Nasrallah <me@abn.sh>
2022-08-03 04:54:37 +0200

    manage gtk settings with Home Manager

--- nix/home.nix
+++ nix/home.nix
@@ -480,6 +480,46 @@ in
         config = ../dotfiles/xmonad/xmonad.hs;
         enableContribAndExtras = true;
       };
+
+      gtk = {
+        enable = true;
+        iconTheme = {
+          name = "breeze";
+        };
+        cursorTheme = {
+          name = "breeze_cursors";
+        };
+        font = {
+          name = "Noto Sans";
+          size = 10;
+        };
+        gtk2.extraConfig = ''
+          gtk-enable-animations=1
+          gtk-primary-button-warps-slider=0
+          gtk-toolbar-style=3
+          gtk-menu-images=1
+          gtk-button-images=1
+          gtk-cursor-theme-size=24
+        '';
+        gtk3.extraConfig = {
+          gtk-application-prefer-dark-theme = false;
+          gtk-button-images = true;
+          gtk-cursor-theme-size = 24;
+          gtk-decoration-layout = "icon:minimize,maximize,close";
+          gtk-enable-animations = true;
+          gtk-menu-images = true;
+          gtk-modules = "colorreload-gtk-module";
+          gtk-primary-button-warps-slider = false;
+          gtk-toolbar-style = 3;
+        };
+        gtk4.extraConfig = {
+          gtk-application-prefer-dark-theme = false;
+          gtk-cursor-theme-size = 24;
+          gtk-decoration-layout = "icon:minimize,maximize,close";
+          gtk-enable-animations = true;
+          gtk-primary-button-warps-slider = false;
+        };
+      };
     })
 
     (lib.mkIf config.myenv.useSway {
