f28cf02482f99ade8567e1a87a65a3cec09c998e
Alexander Ben Nasrallah <me@abn.sh>
2023-06-27 20:37:34 +0200

    nix/home: set XDG variables

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -294,6 +294,17 @@ in
         xsel
       ];
 
+      home.sessionVariables = {
+        XDG_DESKTOP_DIR = "$HOME";
+        XDG_DOCUMENTS_DIR = "$HOME/doc";
+        XDG_DOWNLOAD_DIR = "$HOME/Downloads";
+        XDG_MUSIC_DIR = "$HOME/music";
+        XDG_PICTURES_DIR = "$HOME/pictures";
+        XDG_PUBLICSHARE_DIR = "$HOME/Public";
+        XDG_TEMPLATES_DIR = "$HOME/Templates";
+        XDG_VIDEOS_DIR = "$HOME/Videos";
+      };
+
       xresources.properties = {
         "Xft.autohint" = 0;
         "Xft.lcdfilter" = "lcddefault";
