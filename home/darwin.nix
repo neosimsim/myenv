02aedb058b396be17f6843bd517dbdc52bfae5ca
Alexander Ben Nasrallah <me@abn.sh>
2024-03-16 11:34:29 +0100

    darwin: tweak emacs settings
    Set PATH in Emacs because the nix PATH is not sourced on login.

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,6 +1,16 @@
-{ pkgs, lib, ... }: lib.mkIf pkgs.stdenv.isDarwin {
+{ pkgs, lib, config, ... }: lib.mkIf pkgs.stdenv.isDarwin {
+
+  home.packages = with pkgs; [
+    # Add nix to ensure it matches this flake and so it's added to emacs PATH.
+    nix
+  ];
 
   programs.home-manager.enable = true;
+  programs.emacs.extraConfig = ''
+    (setq explicit-shell-file-name "${config.programs.fish.package}/bin/fish")
+    (setenv "PATH" (concat "${config.home.profileDirectory}/bin:" (getenv "PATH")))
+    (setq exec-path (append '("${config.home.profileDirectory}/bin") exec-path))
+  '';
 
   targets.darwin.search = "DuckDuckGo";
 
