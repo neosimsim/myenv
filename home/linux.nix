43f28cdb4125db2ca9bcf544bcd9eae5dae7efe6
Alexander Ben Nasrallah <me@abn.sh>
2025-06-19 20:41:37 +0200

    linux: Unset XDG variables
    XDG variable are used very inconsistently across application.

--- home/linux.nix
+++ home/linux.nix
@@ -1,16 +1,5 @@
 { pkgs, lib, config, ... }:
 lib.mkIf (pkgs.stdenv.isLinux && config.myenv.coreutils.enable) {
-  home.sessionVariables = {
-    XDG_DESKTOP_DIR = "$HOME";
-    XDG_DOCUMENTS_DIR = "$HOME/doc";
-    XDG_DOWNLOAD_DIR = "$HOME/Downloads";
-    XDG_MUSIC_DIR = "$HOME/music";
-    XDG_PICTURES_DIR = "$HOME/pictures";
-    XDG_PUBLICSHARE_DIR = "$HOME/Public";
-    XDG_TEMPLATES_DIR = "$HOME/Templates";
-    XDG_VIDEOS_DIR = "$HOME/Videos";
-  };
-
   home.packages = with pkgs; [
     racket-minimal
   ];
